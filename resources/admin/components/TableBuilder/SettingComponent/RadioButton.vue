d3aff1777736d579c96018fbac9400ed4b43b264
Naeem Haque <naeemhaque248@gmail.com>
2023-04-10 14:50:11 +0600

    Added feature- Button Size Option

---
+++ resources/admin/components/TableBuilder/SettingComponent/RadioButton.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="radioButton">
+        <span>{{ label }}</span><br>
+        <el-radio-group :value="value" size="small" @input="$emit('update', $event)">
+            <el-radio-button v-for="option in options" :key="option.value" :label="option.label"></el-radio-button>
+        </el-radio-group>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "RadioButton",
+    model: {
+        prop: "value",
+        event: "update"
+    },
+    props: {
+        value: {
+            type: String,
+            default: 'M'
+        },
+        options: {
+            type: Array,
+            default: () => []
+        },
+        event: {
+            type: String,
+            default: 'update'
+        },
+        label: {
+            type: String,
+            default: ""
+        }
+    }
+}
+</script>
+
+<style scoped>
+.radioButton {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-right: 10px;
+}
+</style>
