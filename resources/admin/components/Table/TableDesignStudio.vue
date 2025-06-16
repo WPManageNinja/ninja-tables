c95068d8bdcfe1c0d99d971333c0449acee59d95
NaeemHaque <41508037+NaeemHaque@users.noreply.github.com>
2025-06-16 18:22:57 +0600

    Fixes: Alternate row toggle design

--- resources/admin/components/Table/TableDesignStudio.vue
+++ resources/admin/components/Table/TableDesignStudio.vue
@@ -485,10 +485,10 @@
                                </el-collapse-item>
                              </el-collapse>
 
-                            <el-collapse accordion class="nt-design-collapse mb-5" v-model="activeAlternativeColor">
+                            <el-collapse accordion class="nt-design-collapse mb-5" v-model="activeAlternativeColor" @change="handleAlternativeColorChange">
                                 <el-collapse-item name="alternate_color_status">
                                     <template #title>
-                                        <div>
+                                        <div class="flex items-center">
                                             <el-switch
                                                 size="small"
                                                 v-model="tableSettings.alternate_color_status"
@@ -501,9 +501,8 @@
                                                     e.stopPropagation();
                                                 }"
                                             />
-                                            <span style="font-weight: 500; font-size: 14px;">{{
-                                                    $t('Use Alternate Color Schema for Table Rows')
-                                                }}</span>
+                                            <p class="text-[14px] font-[500]">
+                                                {{ $t('Use Alternate Color Schema for Table Rows') }}</p>
                                         </div>
                                     </template>
 
@@ -535,7 +534,6 @@
                                         </div>
                                     </div>
 
-
                                     <div class="even_row_color_group">
                                         <div class="flex items-center">
                                             <img class="mr-2" :src="assetUrl('icons/layout.svg')"/>
@@ -565,10 +563,6 @@
                                     </div>
                                 </el-collapse-item>
                             </el-collapse>
-
-
-
-                        
 
 <!--                            <div class="ninja_switch_wrapper px-[18px]">-->
 <!--                                <el-switch inactive-color="gray" active-text="Use Alternate Color Schema for Table Rows"-->
@@ -1077,13 +1071,17 @@ export default {
     },
     methods: {
         assetUrl,
-     isStyleActive(styleKey) {
+        isStyleActive(styleKey) {
             if (!Array.isArray(this.tableSettings.css_classes)) {
                 this.tableSettings.css_classes = [];
             }
             return this.tableSettings.css_classes.includes(styleKey);
         },
-        
+        handleAlternativeColorChange(activeNames) {
+            if (this.tableSettings.alternate_color_status !== 'yes' && activeNames.includes('alternate_color_status')) {
+                this.activeAlternativeColor = [];
+            }
+        },
         toggleStyle(styleKey, isActive) {
             if (!Array.isArray(this.tableSettings.css_classes)) {
                 this.tableSettings.css_classes = [];
